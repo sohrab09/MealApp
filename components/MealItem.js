178d5ad5023601ebe59a081a9c31562758a098ce
sohrab09 <sohrab@nassagroup.org>
2022-09-26 18:55:24 +0600

    Passing props from MealsOverview to MealItem

--- components/MealItem.js
+++ components/MealItem.js
@@ -1,10 +1,32 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image, Pressable, StyleSheet } from 'react-native'
 import React from 'react'
 
-export default function MealItem({ title }) {
+export default function MealItem({ title, imageUrl, affordability, complexity, duration }) {
     return (
         <View>
-            <Text>{title}</Text>
+            <Pressable>
+                <View>
+                    <Image source={{ uri: imageUrl }} style={styles.image} />
+                    <Text style={styles.title}>{title}</Text>
+                </View>
+                <View>
+                    <Text>{affordability}</Text>
+                    <Text>{complexity}</Text>
+                    <Text>{duration}</Text>
+                </View>
+            </Pressable>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    image: {
+        width: '100%',
+        height: 200,
+    },
+    title: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+})
