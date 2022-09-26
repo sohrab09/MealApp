e652df7c58e473affdd593ac4becad8a863c0558
sohrab09 <sohrab@nassagroup.org>
2022-09-26 16:26:42 +0600

    Home Page Added

---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,26 @@
+import { FlatList } from 'react-native'
+import React from 'react'
+import { CATEGORIES } from '../data/dummy-data'
+import CategoryGridTile from '../components/CategoryGridTile'
+
+
+function renderCategoryItem(itemData) {
+    return (
+        <CategoryGridTile
+            title={itemData.item.title}
+            color={itemData.item.color}
+        />
+    )
+}
+
+
+export default function CategoriesScreen() {
+    return (
+        <FlatList
+            data={CATEGORIES}
+            renderItem={renderCategoryItem}
+            numColumns={2}
+            keyExtractor={(item) => item.id}
+        />
+    )
+}
