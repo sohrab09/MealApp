b34ab443f87409cc015b9c9b8861c32a3d56e255
sohrab09 <sohrab@nassagroup.org>
2022-09-27 12:04:13 +0600

    Add Star Button in MealDetail Component

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,5 +1,5 @@
 import { useLayoutEffect } from 'react';
-import { View, Text, Image, StyleSheet, ScrollView } from 'react-native';
+import { View, Text, Image, StyleSheet, ScrollView, Button } from 'react-native';
 
 import IconButton from '../components/IconButton';
 import List from '../components/MealDetail/List';
@@ -8,74 +8,74 @@ import MealDetails from '../components/MealDetails';
 import { MEALS } from '../data/dummy-data';
 
 function MealDetailScreen({ route, navigation }) {
-  const mealId = route.params.mealId;
+    const mealId = route.params.mealId;
 
-  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+    const selectedMeal = MEALS.find((meal) => meal.id === mealId);
 
-  function headerButtonPressHandler() {
-    console.log('Pressed!');
-  }
+    function headerButtonPressHandler() {
+        console.log('Pressed!');
+    }
 
-//   useLayoutEffect(() => {
-//     navigation.setOptions({
-//       headerRight: () => {
-//         return (
-//           <IconButton
-//             icon="star"
-//             color="white"
-//             onPress={headerButtonPressHandler}
-//           />
-//         );
-//       },
-//     });
-//   }, [navigation, headerButtonPressHandler]);
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerRight: () => {
+                return (
+                    <IconButton
+                        icon="star"
+                        color="white"
+                        onPress={headerButtonPressHandler}
+                    />
+                );
+            },
+        });
+    }, [navigation, headerButtonPressHandler]);
 
-  return (
-    <ScrollView style={styles.rootContainer}>
-      <Image style={styles.image} source={{ uri: selectedMeal.imageUrl }} />
-      <Text style={styles.title}>{selectedMeal.title}</Text>
-      <MealDetails
-        duration={selectedMeal.duration}
-        complexity={selectedMeal.complexity}
-        affordability={selectedMeal.affordability}
-        textStyle={styles.detailText}
-      />
-      <View style={styles.listOuterContainer}>
-        <View style={styles.listContainer}>
-          <Subtitle>Ingredients</Subtitle>
-          <List data={selectedMeal.ingredients} />
-          <Subtitle>Steps</Subtitle>
-          <List data={selectedMeal.steps} />
-        </View>
-      </View>
-    </ScrollView>
-  );
+    return (
+        <ScrollView style={styles.rootContainer}>
+            <Image style={styles.image} source={{ uri: selectedMeal.imageUrl }} />
+            <Text style={styles.title}>{selectedMeal.title}</Text>
+            <MealDetails
+                duration={selectedMeal.duration}
+                complexity={selectedMeal.complexity}
+                affordability={selectedMeal.affordability}
+                textStyle={styles.detailText}
+            />
+            <View style={styles.listOuterContainer}>
+                <View style={styles.listContainer}>
+                    <Subtitle>Ingredients</Subtitle>
+                    <List data={selectedMeal.ingredients} />
+                    <Subtitle>Steps</Subtitle>
+                    <List data={selectedMeal.steps} />
+                </View>
+            </View>
+        </ScrollView>
+    );
 }
 
 export default MealDetailScreen;
 
 const styles = StyleSheet.create({
-  rootContainer: {
-    marginBottom: 32,
-  },
-  image: {
-    width: '100%',
-    height: 350,
-  },
-  title: {
-    fontWeight: 'bold',
-    fontSize: 24,
-    margin: 8,
-    textAlign: 'center',
-    color: 'white',
-  },
-  detailText: {
-    color: 'white',
-  },
-  listOuterContainer: {
-    alignItems: 'center',
-  },
-  listContainer: {
-    width: '80%',
-  },
+    rootContainer: {
+        marginBottom: 32,
+    },
+    image: {
+        width: '100%',
+        height: 350,
+    },
+    title: {
+        fontWeight: 'bold',
+        fontSize: 24,
+        margin: 8,
+        textAlign: 'center',
+        color: 'white',
+    },
+    detailText: {
+        color: 'white',
+    },
+    listOuterContainer: {
+        alignItems: 'center',
+    },
+    listContainer: {
+        width: '80%',
+    },
 });
