cdaee6746e70d4e34df7b75e2dab4fba036eae0d
sohrab09 <sohrab@nassagroup.org>
2022-09-27 10:16:39 +0600

    Dynamic Header Title Added from MealsOverviewScreen

--- App.js
+++ App.js
@@ -11,9 +11,30 @@ export default function App() {
   return (
     //  <StatusBar style="light" />
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Meals Categories" component={CategoriesScreen} />
-        <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: { backgroundColor: '#351401' },
+          headerTintColor: '#fff',
+          contentStyle: { backgroundColor: '#3f2f25' },
+        }}
+      >
+        <Stack.Screen
+          name="Meals Categories"
+          component={CategoriesScreen}
+          options={{
+            title: 'All Categories',
+          }}
+        />
+        <Stack.Screen
+          name="MealsOverview"
+          component={MealsOverviewScreen}
+          // options={({ route, navigation }) => {
+          //   const catId = route.params.categoryId;
+          //   return {
+          //     title: catId,
+          //   }
+          // }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,15 +1,21 @@
 import { View, FlatList, Text, StyleSheet } from 'react-native'
-import React from 'react'
-import { MEALS } from '../data/dummy-data'
+import React, { useLayoutEffect } from 'react'
+import { MEALS, CATEGORIES } from '../data/dummy-data'
 import MealItem from '../components/MealItem';
 
-export default function MealsOverviewScreen({ route }) {
+export default function MealsOverviewScreen({ route, navigation }) {
 
     const catId = route.params.categoryId
 
     const displayedMeals = MEALS.filter((mealItem) => {
         return mealItem.categoryIds.indexOf(catId) >= 0
     });
+
+    useLayoutEffect(() => {
+        const categoryTitle = CATEGORIES.find((category) => { return category.id === catId }).title;
+        navigation.setOptions({ title: categoryTitle });
+    }, [catId, navigation]);
+
 
     function renderMealItem(itemData) {
 
