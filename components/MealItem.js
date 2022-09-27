e70c97635eed41f66b728de53213ac5e34c697f5
sohrab09 <sohrab@nassagroup.org>
2022-09-27 09:57:28 +0600

    MealItem component design

--- components/MealItem.js
+++ components/MealItem.js
@@ -1,18 +1,24 @@
-import { View, Text, Image, Pressable, StyleSheet } from 'react-native'
+import { View, Text, Image, Pressable, StyleSheet, Platform } from 'react-native'
 import React from 'react'
 
 export default function MealItem({ title, imageUrl, affordability, complexity, duration }) {
     return (
-        <View>
-            <Pressable>
-                <View>
-                    <Image source={{ uri: imageUrl }} style={styles.image} />
-                    <Text style={styles.title}>{title}</Text>
-                </View>
-                <View>
-                    <Text>{affordability}</Text>
-                    <Text>{complexity}</Text>
-                    <Text>{duration}</Text>
+        <View style={styles.mealItem}>
+            <Pressable
+                android_ripple={{ color: '#ccc' }}
+                style={({ pressed }) =>
+                    pressed ? styles.buttonPressed : null
+                }>
+                <View style={styles.innerContainer}>
+                    <View>
+                        <Image source={{ uri: imageUrl }} style={styles.image} />
+                        <Text style={styles.title}>{title}</Text>
+                    </View>
+                    <View style={styles.details}>
+                        <Text style={styles.detailsItem}>{affordability}</Text>
+                        <Text style={styles.detailsItem}>{complexity}</Text>
+                        <Text style={styles.detailsItem}>{duration}</Text>
+                    </View>
                 </View>
             </Pressable>
         </View>
@@ -20,6 +26,23 @@ export default function MealItem({ title, imageUrl, affordability, complexity, d
 }
 
 const styles = StyleSheet.create({
+    mealItem: {
+        margin: 16,
+        borderRadius: 8,
+        overflow: Platform.OS === 'android' ? 'hidden' : 'visible',
+        backgroundColor: 'white',
+        shadowColor: 'black',
+        shadowOpacity: 0.35,
+        shadowOffset: { width: 0, height: 2 },
+        shadowRadius: 16,
+    },
+    innerContainer: {
+        borderRadius: 8,
+        overflow: 'hidden',
+    },
+    buttonPressed: {
+        opacity: 0.5,
+    },
     image: {
         width: '100%',
         height: 200,
@@ -28,5 +51,16 @@ const styles = StyleSheet.create({
         fontSize: 18,
         fontWeight: 'bold',
         textAlign: 'center',
+        margin: 8,
     },
+    details: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 8
+    },
+    detailsItem: {
+        marginHorizontal: 4,
+        fontSize: 14,
+    }
 })
